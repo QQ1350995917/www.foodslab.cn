fb79924ef3a09d7cbf79b6b383b224be0e74ba4c
DingPengwei <www.dingpengwei@gmail.com>
2016-11-08 18:50:35 +0800

    gen order code

--- src/main/java/cn/foodslab/common/utils/OrderCode.java
+++ src/main/java/cn/foodslab/common/utils/OrderCode.java
@@ -1,32 +1,26 @@
 package cn.foodslab.common.utils;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by Pengwei Ding on 2016-11-01 11:27.
  * Email: www.dingpengwei@foxmail.com www.dingpegnwei@gmail.com
- * Description: @TODO
+ * Description: 生成订单编号
+ * 要求：1：非零开头的十一位数字字符串，2：防止预测生成总量和单位时间生成总量，3：尽可能不重复，4：速度每秒最低生成100个
  */
 public class OrderCode {
-    private static Date CURRENT_DATE = new Date();
-    private static int CURRENT_BASE_CODE = 0;
-    static{
-        String suffix = String.format("%02d", Calendar.getInstance().get(Calendar.HOUR_OF_DAY))
-                + String.format("%02d", Calendar.getInstance().get(Calendar.MINUTE));
-    }
-
     public static synchronized String gen() {
-        if (!new Date().equals(CURRENT_DATE)) {
-            //每天更换新的基数号码
-            CURRENT_DATE = new Date();
-            CURRENT_BASE_CODE = 0;
-        }
-        Random random = new Random(9999);
-        int nextInt = random.nextInt();
-        int currentOrderCode = nextInt + CURRENT_BASE_CODE;
-        CURRENT_BASE_CODE ++;
-        return currentOrderCode + "";
+        Random random = new Random();
+        return (random.nextInt(9) + 1) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + ""
+                + random.nextInt(9) + "";
     }
 }
